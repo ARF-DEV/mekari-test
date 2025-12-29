728c0de32f98b188b31c4aacedba5272767a7272
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2025-12-29 12:36:15 +0700

    feat: add user seed

---
+++ migration/000004_seed_users.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (email, name, role, created_at) VALUES
+    ('testing-manager@example.com', 'test-manager', 'manager', now()),
+    ('testing-user@example.com', 'test-user', 'user', now());
